9734e7ef2ba561c80de3df5239ac83cd79d11244
William Smith <williamjsmith15@hotmail.co.uk>
2026-03-14 04:34:54 +0000

    Add Galaxy tool registry and Docker build script

---
+++ galaxy/build-push.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+ORG="williamjsmith15"
+DATETIME=$(date +%Y%m%d-%H%M%S)
+
+build_and_push() {
+    local name=$1
+    local dockerfile=$2
+
+    docker build -t "$name:build" -f "$dockerfile" .
+
+    for tag in latest "$DATETIME"; do
+        docker tag "$name:build" "$ORG/$name:$tag"
+        docker push "$ORG/$name:$tag"
+    done
+
+    docker rmi "$name:build"
+}
+
+build_and_push parafem-meshgen     contianers/Dockerfile.meshgen
+build_and_push parafem-p123        contianers/Dockerfile.p123
+build_and_push parafem-bcgen       contianers/Dockerfile.bcgen
+build_and_push parafem-postprocess contianers/Dockerfile.postprocess
