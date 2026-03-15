ac28cc475cf0311e27b3d04a53c5abdd990dcf17
William Smith <williamjsmith15@hotmail.co.uk>
2026-03-15 07:56:41 +0000

    Harden solver and postprocess tools for robust output handling

--- galaxy/build-push.sh
+++ galaxy/build-push.sh
@@ -22,3 +22,4 @@ build_and_push parafem-meshgen     contianers/Dockerfile.meshgen
 build_and_push parafem-p123        contianers/Dockerfile.p123
 build_and_push parafem-bcgen       contianers/Dockerfile.bcgen
 build_and_push parafem-postprocess contianers/Dockerfile.postprocess
+build_and_push parafem-vtu         contianers/Dockerfile.vtu
